64f6e75e25eafb4f4a5627d5e05cfc241f5a9102
Rajarshi <rajarshi.ganguly@telerys.fr>
2025-04-10 11:19:26 +0200

    variables changed to the env file

--- scripts/db_backups.sh
+++ scripts/db_backups.sh
@@ -2,6 +2,13 @@
 
 # setting the vars
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
+RETURN_CODE=0
+
+if [ ! -f "$(dirname "$0")/.env" ]; then RETURN_CODE=1; critical "No environment file, use the sample file"; fi
+set -a
+
+source "$(dirname "$0")/.env"
+set +a
 
 # Create backup directory if not exists
 sudo mkdir -p "$BACKUP_DIR"
