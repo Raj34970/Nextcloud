c5c73bf1b4b4e974411967cb70b9cad57f1f9a9e
rajarshi <rajarshi.ganguly@telerys.com>
2025-10-27 11:47:27 +0100

    added: import of .env variables.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash 
+
+RETURN_CODE=0
+
+# loading the .env file
+if [ ! -f "$(dirname "$0")/.env" ]; then RETURN_CODE=1; critical "No environment file, use the sample file"; fi
+set -a
+source "$(dirname "$0")/.env"
+set +a
+
 echo "Cleaning up the container backup from $BACKUP_DIR"
 sudo docker exec rm -rf "$NEXTCLOUD_CONTAINER":"$BACKUP_DIR"/*
